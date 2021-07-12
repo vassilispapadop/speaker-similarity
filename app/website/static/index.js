c2d896a4e88f96cbe31e718f4e962066a53a5f4f
Vassilis <vassilispapadop@gmail.com>
2021-07-12 18:58:35 +0300

    onclick=playSound

---
+++ app/website/static/index.js
@@ -0,0 +1,11 @@
+function playSound(clip){
+    clip = '/Users/vpapadop/Documents/GitHub/speaker-similarity/app/website/downloads/parts/output000000001.wav'
+    var audio = new Audio(clip);
+    audio.play();
+    // fetch('/delete-note', {
+    //     method: 'POST',
+    //     body: JSON.stringify({noteId: noteId})
+    // }).then((_res) => {
+    //     window.location.href ="/"
+    // }); 
+}
