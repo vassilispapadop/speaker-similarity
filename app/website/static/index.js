a4a067e49d9716ce6afddf54684283f61cb60459
Vassilis <vassilispapadop@gmail.com>
2021-07-14 12:59:33 +0300

    record audio from web cam

--- app/website/static/index.js
+++ app/website/static/index.js
@@ -1,32 +1,41 @@
-var audio;
+const recordAudio = () =>
+  new Promise(async resolve => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const mediaRecorder = new MediaRecorder(stream);
+    const audioChunks = [];
 
-function playSound(clip){
-    // clip = '/Users/vpapadop/Documents/GitHub/speaker-similarity/app/website/downloads/parts/output000000000.wav'
-    // var audio = new Audio("https://s3.amazonaws.com/audio-experiments/examples/elon_mono.wav")
-    // file = 'listen/?s='+clip
-    // audio = new Audio(file)
-    // audio.play();
+    mediaRecorder.addEventListener("dataavailable", event => {
+      audioChunks.push(event.data);
+    });
 
-    // fetch('/listen', {
-    //     method: 'POST',
-    //     body: JSON.stringify({source: clip})
-    // }).then((res) => {
-    //     console.log(res)
-    //     // window.location.href ="/"
-    // }); 
+    const start = () => mediaRecorder.start();
 
+    const stop = () =>
+      new Promise(resolve => {
+        mediaRecorder.addEventListener("stop", () => {
+          const audioBlob = new Blob(audioChunks);
+          const audioUrl = URL.createObjectURL(audioBlob);
+          const audio = new Audio(audioUrl);
+          const play = () => audio.play();
+          resolve({ audioBlob, audioUrl, play });
+        });
 
-    fetch('/listen', {
-        method: 'POST',
-        body: JSON.stringify({source: clip})
-    }).then(response => response.blob()).then(blob => {
-        let file = new File([blob], clip, {
-                    type:"audio/x-wav", lastModified:new Date().getTime()
-            });
-            // do stuff with `file`
-            audio = new Audio(file)
-            audio.play()
-        }).catch(err => console.error(err));
+        mediaRecorder.stop();
+      });
 
+    resolve({ start, stop });
+  });
 
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+
+const handleAction = async () => {
+  const recorder = await recordAudio();
+  const actionButton = document.getElementById('action');
+  actionButton.disabled = true;
+  recorder.start();
+  await sleep(3000);
+  const audio = await recorder.stop();
+  audio.play();
+  await sleep(3000);
+  actionButton.disabled = false;
 }
