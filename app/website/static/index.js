413d15d38acc96465c340f526d4a205b0352e427
Vassilis <vassilispapadop@gmail.com>
2021-07-15 11:57:13 +0300

    upload recording and make predicton

--- app/website/static/index.js
+++ app/website/static/index.js
@@ -1,41 +1,183 @@
-const recordAudio = () =>
-  new Promise(async resolve => {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    const mediaRecorder = new MediaRecorder(stream);
-    const audioChunks = [];
+URL = window.URL || window.webkitURL;
 
-    mediaRecorder.addEventListener("dataavailable", event => {
-      audioChunks.push(event.data);
+var gumStream;                      //stream from getUserMedia()
+var rec;                            //Recorder.js object
+var input;                          //MediaStreamAudioSourceNode we'll be recording
+
+// shim for AudioContext when it's not avb. 
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext //audio context to help us record
+
+var recordButton = document.getElementById("recordButton");
+var stopButton = document.getElementById("stopButton");
+// var pauseButton = document.getElementById("pauseButton");
+
+//add events to those 2 buttons
+recordButton.addEventListener("click", startRecording);
+stopButton.addEventListener("click", stopRecording);
+// pauseButton.addEventListener("click", pauseRecording);
+
+function startRecording() {
+    console.log("recordButton clicked");
+
+    /*
+        Simple constraints object, for more advanced audio features see
+        https://addpipe.com/blog/audio-constraints-getusermedia/
+    */
+
+    var constraints = { audio: true, video:false }
+
+    /*
+        Disable the record button until we get a success or fail from getUserMedia() 
+    */
+
+    recordButton.disabled = true;
+    stopButton.disabled = false;
+    // pauseButton.disabled = false
+
+    /*
+        We're using the standard promise based getUserMedia() 
+        https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+    */
+
+    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+        console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+
+        /*
+            create an audio context after getUserMedia is called
+            sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
+            the sampleRate defaults to the one set in your OS for your playback device
+
+        */
+        audioContext = new AudioContext();
+
+        //update the format 
+        document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
+
+        /*  assign to gumStream for later use  */
+        gumStream = stream;
+
+        /* use the stream */
+        input = audioContext.createMediaStreamSource(stream);
+
+        /* 
+            Create the Recorder object and configure to record mono sound (1 channel)
+            Recording 2 channels  will double the file size
+        */
+        rec = new Recorder(input,{numChannels:1})
+
+        //start the recording process
+        rec.record()
+
+        console.log("Recording started");
+
+    }).catch(function(err) {
+        //enable the record button if getUserMedia() fails
+        recordButton.disabled = false;
+        stopButton.disabled = true;
+        // pauseButton.disabled = true
     });
+}
 
-    const start = () => mediaRecorder.start();
+// function pauseRecording(){
+//     console.log("pauseButton clicked rec.recording=",rec.recording );
+//     if (rec.recording){
+//         //pause
+//         rec.stop();
+//         pauseButton.innerHTML="Resume";
+//     }else{
+//         //resume
+//         rec.record()
+//         pauseButton.innerHTML="Pause";
 
-    const stop = () =>
-      new Promise(resolve => {
-        mediaRecorder.addEventListener("stop", () => {
-          const audioBlob = new Blob(audioChunks);
-          const audioUrl = URL.createObjectURL(audioBlob);
-          const audio = new Audio(audioUrl);
-          const play = () => audio.play();
-          resolve({ audioBlob, audioUrl, play });
-        });
+//     }
+// }
 
-        mediaRecorder.stop();
-      });
+function stopRecording() {
+    console.log("stopButton clicked");
 
-    resolve({ start, stop });
-  });
+    //disable the stop button, enable the record too allow for new recordings
+    stopButton.disabled = true;
+    recordButton.disabled = false;
+    // pauseButton.disabled = true;
 
-const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+    //reset button just in case the recording is stopped while paused
+    // pauseButton.innerHTML="Pause";
 
-const handleAction = async () => {
-  const recorder = await recordAudio();
-  const actionButton = document.getElementById('action');
-  actionButton.disabled = true;
-  recorder.start();
-  await sleep(3000);
-  const audio = await recorder.stop();
-  audio.play();
-  await sleep(3000);
-  actionButton.disabled = false;
+    //tell the recorder to stop the recording
+    rec.stop();
+
+    //stop microphone access
+    gumStream.getAudioTracks()[0].stop();
+
+    //create the wav blob and pass it on to createDownloadLink
+    rec.exportWAV(createDownloadLink);
+}
+number_of_recordings = 0
+function createDownloadLink(blob) {
+
+    var url = URL.createObjectURL(blob);
+    var au = document.createElement('audio');
+    var li = document.createElement('li');
+    var link = document.createElement('a');
+
+    //name of .wav file to use during upload and download (without extendion)
+    // var date = new Date();
+    // date.setMilliseconds(0);
+    // var dateISOString = date.toISOString().replace(".000Z", "Z");
+
+    var today = new Date();
+    var year = today.getFullYear();
+    var mes = today.getMonth()+1;
+    var dia = today.getDate();
+    var fecha =dia+"-"+mes+"-"+year+"-"+number_of_recordings;
+    number_of_recordings +=1
+    // console.log(dateISOString);
+    var filename = fecha;
+
+    //add controls to the <audio> element
+    au.controls = true;
+    au.src = url;
+
+    //save to disk link
+    // link.href = url;
+    // link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
+    // link.innerHTML = "Save to disk";
+
+    //add the new audio element to li
+    li.appendChild(au);
+
+    //add the filename to the li
+    li.appendChild(document.createTextNode(filename+".wav "))
+
+    //add the save to disk link to li
+    // li.appendChild(link);
+
+    //upload link
+    var upload = document.createElement('a');
+    upload.href="#";
+    upload.innerHTML = "Upload";
+    upload.addEventListener("click", function(event){
+          var xhr=new XMLHttpRequest();
+          xhr.onload=function(e) {
+              if(this.readyState === 4) {
+                    console.log("Server returned: ",e.target.responseText);
+                    response = JSON.parse(e.target.responseText)
+                    var form = '<form name="myform" method="post" hidden="true">' +
+                                '<input type="text" name="upload-id" id="upload-id" value="'+ response.title + '" />' +
+                                '</form>';
+                    document.body.innerHTML += form;
+                    document.forms.myform.submit();
+              }
+          };
+          var fd=new FormData();
+          fd.append("audio_data",blob, filename);
+          xhr.open("POST","/upload",true);
+          xhr.send(fd);
+    })
+    li.appendChild(document.createTextNode (" "))//add a space in between
+    li.appendChild(upload)//add the upload link to li
+
+    //add the li element to the ol
+    recordingsList.appendChild(li);
 }
--- app/website/views.py
+++ app/website/views.py
@@ -1,44 +1,55 @@
 # routes of website besides auth page
-from flask import Blueprint, render_template, request, flash, jsonify, send_file
+from flask import Blueprint, render_template, request,redirect, flash, jsonify, make_response
 from .process_audio import download_audio, create_segments, predict
 from . import metadata
+import os
 import json
 
 PATH_TO_CLIPS = 'downloads/parts/'
 
 views = Blueprint('views', __name__)
 
+def make_predict(file, segment_duration):
+    # crops file into segment_duration-seconds chunks
+    segments = create_segments(path = file, segment_duration = segment_duration)
+    pred_dict = predict(segments)
+
+    return pred_dict
+
 @views.route('/', methods=['GET','POST'])
 def home():
     segments = []
     if request.method == 'POST':
-        url = request.form.get('note')          
-        if not url:
-            return render_template("home.html")
+        file = None
+        url = request.form.get('yb-url')
+        upload = request.form.get('upload-id')
+        if url:
+            file = download_audio(url)
+            print(f'Downloaded file: {file}')
+        else:
+            file = upload
 
-        file = download_audio(url)
-        print(f'Downloaded file: {file}')
-
-        # crops file into 10-seconds chunks
-        segments = create_segments(path = file, segment_duration = 10)
-        pred_dict = predict(segments)
-        
+        pred_dict = make_predict(file=file, segment_duration=10)
         return render_template("segments.html", title = file.rsplit('/', 1)[-1],  pred_dict=pred_dict)
     else:    
-        return render_template("home.html", speakers=metadata['VGGFace1 ID'].to_dict())
+        return render_template("home.html")
+
 
 @views.route('/celebrities', methods=['GET'])
 def celebrities():
     return render_template("celebrities.html", speakers=metadata['VGGFace1 ID'].to_dict())
 
-@views.route('/listen', methods=['POST'])
-def listen():
-    data = json.loads(request.data)
-    # path = data['source']
-    # fileName = data['source'].rsplit('/', 1)[-1]
-    
-    return send_file(
-        PATH_TO_CLIPS+data['source'], 
-        mimetype="audio/wav", 
-        as_attachment=True, 
-        attachment_filename=data['source'])
+
+@views.route('/upload', methods=['POST'])
+def upload():
+    if request.method == "POST":
+        print(request.files)
+        
+        f = request.files['audio_data']
+        file = f.filename + '.wav'
+        with open(file, 'wb') as audio:
+            f.save(audio)
+        return jsonify(dict(redirect='segments', title=file))
+        
+   
+    return render_template("home.html")
