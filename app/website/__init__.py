84232fa2b59d6059ad455a2e24b3e5ed79a2f1a1
Vassilis <vassilispapadop@gmail.com>
2021-07-16 11:47:42 +0300

    reduced dropout to 0.3 and increased batch_size

--- app/website/__init__.py
+++ app/website/__init__.py
@@ -22,20 +22,21 @@ gmm_model = pickle.load(open(saved_models_path + GMM_MODEL_NAME, 'rb'))
 
 # Load classes file, order of speakers is preserved
 classes = np.load(saved_models_path + 'classes.npy', allow_pickle=True)
-print(classes)
 
 # Load metadata csv file to map id to actual person
 csv_path = os.path.join(cwd, 'vox_dev_wav/')
 metadata = pd.read_csv(csv_path + 'vox1_meta.csv', sep='\t')
 metadata = metadata.drop(['Gender',	'Nationality',	'Set'], axis=1)
 metadata = metadata[metadata['VoxCeleb1 ID'].isin(classes)]
-print(f'Current speakers shape: {metadata.shape}')
+
 
 def create_app():
     #name of the file initialize flask
     app = Flask(__name__)
 
     print(nn_model.summary())
+    print(f'Classes are: {classes}')
+    print(f'Current speakers shape: {metadata.shape}')
 
     # register views/routes
     from .views import views
